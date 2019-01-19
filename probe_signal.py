2163dfe3969985c72424dabba64e7c80bb516b0a
nedlrichards <edwardlrichards@gmail.edu>
2019-01-19 14:53:28 -0800

    added a narrowbanded probe signal

--- probe_signal.py
+++ probe_signal.py
@@ -12,15 +12,15 @@ class Probe:
         self.bw = bw
         self.fs = fs
         self.period = period
-        num_samples = period * fs
+        num_samples = int(np.round(period * fs))
         self.time = np.arange(num_samples) / fs
         self.signal = np.zeros(self.time.shape)
 
     def FT(self):
         """Compute the signal's FT"""
         NFFT = int(2 ** np.log2(np.ceil(self.signal.size) + 1))
-        FT = np.fft.fft(self.signal, NFFT)
-        f = np.arange(NFFT) / NFFT * self.fs
+        FT = np.fft.rfft(self.signal, NFFT)
+        f = np.arange(NFFT / 2) / NFFT * self.fs
         return (FT, f)
 
     def auto_corr(self):
@@ -58,7 +58,7 @@ class LFM:
         chirp[t_i] = np.sin(2 * np.pi * (f1 * t[t_i] +
                                     k / 2 * t[t_i] ** 2))
         # window?
-        window = sig.kaiser(sum(t_i), 1.5 * np.pi)
+        window = sig.kaiser(sum(t_i), 2.0 * np.pi)
         chirp[t_i] *= window
 
         return chirp
@@ -71,17 +71,55 @@ class LFM:
         """Inherit pulse method"""
         return self.pulse.auto_corr()
 
-    def write_wave(self, file_name):
-        """Composition method"""
-        self.pulse.write_wave(file_name)
+class NarrowBand:
+    """A narrow banded pulse"""
+    def __init__(self, fc, bw, fs):
+        """Duty cycle is length pulse is active"""
+        period = 1 / bw  # basic fourier transform uncertainty
+        num_samples = int(np.ceil(period * fs))
+        # make even
+        if num_samples % 2:
+            num_samples += 1
+        period = num_samples / fs
+        self.pulse = Probe(fc, bw, period, fs)
+        signal = self.narrow_band()
+        self.pulse.signal = signal
+
+    @property
+    def signal(self):
+        """convience"""
+        return self.pulse.signal
+
+    @property
+    def time(self):
+        """convience"""
+        return self.pulse.time
+
+    def narrow_band(self):
+        """create an narrow banded pulse to specs"""
+        xmitt = np.sin(2 * np.pi * self.pulse.fc * self.time)
+        # window is unknown, assuming a pretty narrow mainlobe
+        window = sig.kaiser(self.time.size, 1.0 * np.pi)
+        xmitt *= window
+        return xmitt
+
+    def FT(self):
+        """Inherit pulse method"""
+        return self.pulse.FT()
+
+    def auto_corr(self):
+        """Inherit pulse method"""
+        return self.pulse.auto_corr()
+
 
 if __name__ == "__main__":
-    fc = 7000  # Hz
+    fc = 5000  # Hz
     bw = 12000  # Hz
-    fs = 44.1e3  # Hz
+    fs = 88.2e3  # Hz
     duty_cycle = 0.5
     T = 0.6  # second
-    lfm = LFM(duty_cycle, fc, bw, T, fs)
+    #lfm = LFM(duty_cycle, fc, bw, T, fs)
+    lfm = NarrowBand(fc, 500, fs)
     FT, freq = lfm.FT()
     a_corr, t_corr = lfm.auto_corr()
 
